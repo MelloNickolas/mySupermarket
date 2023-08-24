66edd1ff833ae9bc530a684988ff2ac3ea4843d2
MelloNickolas <nickolasphmello10@gmail.com>
2023-08-24 09:02:33 -0300

    Implementention of my logo

--- supermarketApp/src/pages/Supermarket/index.tsx
+++ supermarketApp/src/pages/Supermarket/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { Text } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import logo from "../../../assets/logo.png"
+
 export default function Supermarket() {
     return (
-
-        <Text></Text>
+        <View>
+            <Image source={logo}/>
+        </View>
 
     )
 }
+
+
+// #4ECCA3 cor verde da logo
+// #393E46 cor preta da logo
