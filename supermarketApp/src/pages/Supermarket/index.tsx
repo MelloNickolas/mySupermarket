0a94d0c5a272057251f4619089143aebc4d7101e
MelloNickolas <nickolasphmello10@gmail.com>
2023-09-06 15:10:28 -0300

    Mexendo nos img

--- supermarketApp/src/pages/Supermarket/index.tsx
+++ supermarketApp/src/pages/Supermarket/index.tsx
@@ -1,51 +1,32 @@
 import React from "react";
+
 import {
   Text,
   View,
   Image,
   StyleSheet,
   Dimensions,
-  SectionList,
 } from "react-native";
 import logo from "../../../assets/logo.png";
+import bolacha from "../../../assets/Produtos/bolacha.png";
 
-const DATA = [
-  {
-    title: "FRIOS",
-    data: ["Mussarela", "Presunto", "Mortadela"],
-  },
-  {
-    title: "Sides",
-    data: ["French Fries", "Onion Rings", "Fried Shrimps"],
-  },
-  {
-    title: "Drinks",
-    data: ["Water", "Coke", "Beer"],
-  },
-  {
-    title: "Desserts",
-    data: ["Cheese Cake", "Ice Cream"],
-  },
-];
+
 
 export default function Supermarket() {
   return (
     <>
-      <View style={styles.back}>
+
+      <View>
         <Image source={logo} style={styles.topo} />
-        <SectionList
-          sections={DATA}
-          keyExtractor={(item, index) => item + index}
-          renderItem={({ item }) => (
-            <View style={styles.item}>
-              <Text style={styles.title}>{item}</Text>
-            </View>
-          )}
-          renderSectionHeader={({ section: { title } }) => (
-            <Text style={styles.header}>{title}</Text>
-          )}
-        />
+
+      <View style={styles.container}>
+        <Text>Mussarela</Text>
+        <Text>R$37,90</Text>
+        <Image source={bolacha} style={styles.imgContainer}/>
       </View>
+
+      </View>
+
     </>
   );
 }
@@ -53,26 +34,18 @@ export default function Supermarket() {
 const width = Dimensions.get("screen").width;
 
 const styles = StyleSheet.create({
-  back: {
-   // backgroundColor: "#000000",
-  },
   topo: {
     width: "100%",
     height: 100,
   },
-  header: {
-    color: "#ed8b00",
-    fontSize: 40,
-    textAlign: "center",
+  container: {
+    width: "100%",
+    display: "flex",
+    justifyContent: "space-between",
   },
-  title: {
-    fontFamily: "Caveat",
-    fontSize: 30,
-  },
-  item: {
-    padding: 20,
-    backgroundColor: "blue"
-  },
+  imgContainer: {
+    height: 100
+  }
 });
 
 // color image yellow #ed8b00
