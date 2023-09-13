ffd2f8e43c78c7453553369159dde0e502e0c01e
MelloNickolas <nickolasphmello10@gmail.com>
2023-09-13 08:18:04 -0300

    Finish this project

--- supermarketApp/src/pages/Supermarket/index.tsx
+++ supermarketApp/src/pages/Supermarket/index.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 
-import { Text, View, Image, StyleSheet, Dimensions } from "react-native";
+import { Text, View, Image, StyleSheet,  ScrollView, Dimensions } from "react-native";
 import logo from "../../../assets/logo.png";
-import minilogo from "../../../assets/minilogo.png";
-import mussarela from "../../../assets/mussarela.png.png";
+import mussarela from "../../../assets/frios/mussarela.png";
+import presunto from "../../../assets/frios/presunto.png";
+import mortadela from "../../../assets/frios/mortadela.png";
 
 export default function Supermarket() {
   return (
     <>
+    
       <View style={styles.back}>
         <Image source={logo} style={styles.topo} />
 
-        <View style={styles.containerImg}>
 
-          <Image source={mussarela} style={styles.imgContainer}/>
+        <View style={styles.containerTitle}>
+          <Text style={styles.title}>FRIOS</Text>
+        </View>
+
+        <View style={styles.containerLinha}>
+          <View style={styles.linha}>
+            <Text>.</Text>
+          </View>
+        </View>
+
+        <View style={styles.containerImg}>
+          <Image source={mussarela} style={styles.imgContainer} />
 
           <View style={styles.container}>
             <View style={styles.start}>
@@ -26,7 +38,34 @@ export default function Supermarket() {
           </View>
         </View>
 
-       
+        <View style={styles.containerImg}>
+          <Image source={presunto} style={styles.imgContainer} />
+
+          <View style={styles.container}>
+            <View style={styles.start}>
+              <Text style={styles.text}>Presunto</Text>
+            </View>
+
+            <View style={styles.end}>
+              <Text style={styles.text}>R$32,40 KG</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.containerImg}>
+          <Image source={mortadela} style={styles.imgContainer} />
+
+          <View style={styles.container}>
+            <View style={styles.start}>
+              <Text style={styles.text}>Mortadela</Text>
+            </View>
+
+            <View style={styles.end}>
+              <Text style={styles.text}>R$26,00 KG</Text>
+            </View>
+          </View>
+        </View>
+     
       </View>
     </>
   );
@@ -35,20 +74,46 @@ export default function Supermarket() {
 const width = Dimensions.get("screen").width;
 
 const styles = StyleSheet.create({
-  back:{
- 
+  back: {},
+  containerTitle: {
+    display: "flex",
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    height: 50,
+    width: "100%",
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 30,
+    color: "#ed8b00",
+    fontWeight: "bold",
+    fontFamily: "Caveat",
   },
   topo: {
     width: "100%",
-    height: 100,
+    height: 90,
+  },
+  containerLinha: {
+    display: "flex",
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+    width: "80%",
+    height: 2,
+  
+  },
+  linha: {
+    backgroundColor: "#11111f",
+    width: "100%",
   },
   containerImg: {
     height: 200,
-    marginVertical: 20,
+    marginVertical: 5,
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 10
+    borderRadius: 10,
   },
   container: {
     display: "flex",
@@ -56,13 +121,14 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     backgroundColor: "#11111f",
     marginHorizontal: 40,
-    height: 50,
-    borderRadius: 10
+    height: 
+    40,
+    borderRadius: 10,
   },
 
   imgContainer: {
     width: "87%",
-    height: "65%",
+    height: "62%",
   },
 
   start: {
@@ -84,7 +150,7 @@ const styles = StyleSheet.create({
     color: "#ed8b00",
     fontFamily: "Caveat",
     fontSize: 20,
-    fontWeight: "bold"
+    fontWeight: "bold",
   },
 });
 
